2cac3bd262df698686a392fa493c366c67e5d657
felipeversiane <felipeversiane09@gmail.com>
2024-01-10 20:49:02 -0300

    create transaction model

---
+++ src/myapp/models/TransactionModel.py
@@ -0,0 +1,23 @@
+from django.db import models
+from rest_framework import serializers
+from .UserModel import CustomUser
+from myapp.validators import *
+
+class Transaction(models.Model):
+    amount = models.DecimalField(verbose_name="Transaction Amount")
+    payer = models.ForeignKey(CustomUser, null=False, blank=False,verbose_name="Transaction Payer")
+    payee = models.ForeignKey(CustomUser, null=False, blank=False,verbose_name="Transaction Payee")
+    transaction_date = models.DateTimeField(auto_now_add=True,verbose_name="Date and Time")
+
+    def __str__(self):
+        return "{}-{}-{}-{}".format(self.payer.username, self.payee.username, self.amount,self.timestamp.strftime('%d-%m-%Y %H:%M'))
+    
+    class Meta:
+        verbose_name = "Transaction"
+        verbose_name_plural = "Transactions"
+        ordering = ['id']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
