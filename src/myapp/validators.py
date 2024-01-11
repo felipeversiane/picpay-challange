c0fd13219bbd7b0d8b9445f4dd43b307aa522187
felipeversiane <felipeversiane09@gmail.com>
2024-01-10 22:48:17 -0300

    feat: create validators

---
+++ src/myapp/validators.py
@@ -0,0 +1,32 @@
+import datetime
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+import re
+from django.core.exceptions import ValidationError
+
+
+def validate_first_letter(str):    
+    if str[0].isdigit():        
+        raise ValidationError("First letter cannot be a number.")
+
+def validate_letters(name):    
+    if not re.match(r'^[A-Za-z ]+$', name):      
+        raise ValidationError("Only letters and spaces allowed.")
+
+def validate_value(value):
+    if value <= 0:
+        raise ValidationError("Value must be positive.")
+        
+def validate_date(date): 
+    if date < timezone.now().date():  
+        raise ValidationError("Invalid date.")
+        
+def validate_time(time):       
+    if time < timezone.now().time():   
+        raise ValidationError("Invalid time.") 
+    
+def validate_transaction(user,amount):
+    if user.balance < amount:
+        raise ValidationError("Balance lower than necessary.")
+    if user.user_type == 'M':
+        raise ValidationError("Merchant cannot make payments")
