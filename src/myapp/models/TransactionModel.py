7c8b3e438ab1ab5d6372b67aa771692a8b5972d0
felipeversiane <felipeversiane09@gmail.com>
2024-01-10 22:48:07 -0300

    feat:  add the transaction manager on transaction model

--- src/myapp/models/TransactionModel.py
+++ src/myapp/models/TransactionModel.py
@@ -2,16 +2,19 @@ from django.db import models
 from rest_framework import serializers
 from .UserModel import CustomUser
 from myapp.validators import *
+from myapp.managers import TransactionManager 
 
 class Transaction(models.Model):
     amount = models.DecimalField(verbose_name="Transaction Amount")
-    payer = models.ForeignKey(CustomUser, null=False, blank=False,verbose_name="Transaction Payer")
-    payee = models.ForeignKey(CustomUser, null=False, blank=False,verbose_name="Transaction Payee")
-    transaction_date = models.DateTimeField(auto_now_add=True,verbose_name="Date and Time")
+    payer = models.ForeignKey(CustomUser, null=False, blank=False, verbose_name="Transaction Payer")
+    payee = models.ForeignKey(CustomUser, null=False, blank=False, verbose_name="Transaction Payee")
+    transaction_date = models.DateTimeField(auto_now_add=True, verbose_name="Date and Time")
+
+    objects = TransactionManager()  
 
     def __str__(self):
-        return "{}-{}-{}-{}".format(self.payer.username, self.payee.username, self.amount,self.timestamp.strftime('%d-%m-%Y %H:%M'))
-    
+        return "{}-{}-{}-{}".format(self.payer.username, self.payee.username, self.amount, self.transaction_date.strftime('%d-%m-%Y %H:%M'))
+
     class Meta:
         verbose_name = "Transaction"
         verbose_name_plural = "Transactions"
